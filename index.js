bd66ecc4da132a24022cf4876e0837739b26f371
Tarik <t2.macben@gmail.com>
2025-05-16 22:24:52 +0800

    Creation of files 404.html and 404.css + code added in index.js for linking the 404.html file

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
+// Variables :
 const express = require('express');
 const path = require('path');
 const port = 5000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.urlencoded({extended:true}));
 
+// Method get :
 app.get('/contact', (req, res)=>{
     res.sendFile(path.join(__dirname, 'views/contact.html'));
 });
 
+// Method post :
 app.post('/form', (req, res)=>{
+    const {email, password} = req.body;
     res.send(`Welcome to your user page !`);
 });
 
+// 404 page :
+app.use((req, res)=>{
+    res.sendFile(path.join(__dirname, 'views/404.html'));
+});
+
+// Launch the server :
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`);
 });
