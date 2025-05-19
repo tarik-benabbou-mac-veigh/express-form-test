cc4b2243307482d22d693ef76de55be0befe7f5c
Tarik <t2.macben@gmail.com>
2025-05-19 21:42:53 +0800

    Creation of a condition for the password to be enterred on the /contact url + a profile.js to create an alert once well connected to the /form url

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ app.get('/contact', (req, res)=>{
 // Method post :
 app.post('/form', (req, res)=>{
     const {email, password} = req.body;
-    res.sendFile(path.join(__dirname, 'views/profile.html'));
+    if(password !== 'JavaScript'){
+        res.send('-- Access denied --');
+    }else{
+        res.sendFile(path.join(__dirname, 'views/profile.html'));
+    }
 });
 
 // 404 page :
